ad2a969ed62881838c8de5d9ca9e49f69e0d7cec
Lanyi-adict <Lanyi_adict@outlook.com>
2024-10-30 20:04:54 +0800

    Delete Java directory

--- Java/Program_0/compute.java
+++
@@ -1,9 +0,0 @@
-package Program_0;
-
-public class compute {
-    public static void main(String[] args) {
-        int sum = 0;
-        sum= 1 + 3 + 5 + 7 + 9;
-        System.out.println("1 + 3 + 5 + 7 + 9 = " + sum);
-    }
-}
