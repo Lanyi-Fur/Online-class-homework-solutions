b51601b476cf3ffe0eed6e1fc02ee90b6b158f34
Lanyi-adict <Lanyi_adict@outlook.com>
2024-10-31 23:25:34 +0800

    Delete Java exam in SXIT directory

--- Java exam in SXIT/Program_2/MyDatademo/src/MyDateDemo.java
+++
@@ -1,5 +0,0 @@
-public class MyDateDemo {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
--- Java exam in SXIT/README.md
+++
@@ -1,2 +0,0 @@
-# Introduction
-Yes, that's all the beauty of the experiments and solutions in SXIT's Java Programming course.
